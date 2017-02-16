23a6378acb1f142894cae1a60a4fa2122d06dbad
pattysc <pattys@users.noreply.github.com>
2017-02-16 11:53:45 -0500

    logout button

--- src/components/CommuteApp.js
+++ src/components/CommuteApp.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
 
 class CommuteApp extends Component {
+
+  handleLogOut(){
+    sessionStorage.clear()
+    browserHistory.push('/')
+  }
 
   render(){
     return(
       <div className="Commute">
+        <button value="Log Out" onClick={this.handleLogOut.bind(this)}>Log Out</button><br/>
         { this.props.children }
       </div>
 
--- src/components/ProfileApp.js
+++ src/components/ProfileApp.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
 
 class ProfileApp extends Component {
+
+  handleLogOut(){
+    sessionStorage.clear()
+    browserHistory.push('/')
+  }
 
   render(){
     return(
       <div className="Profile">
+        <button value="Log Out" onClick={this.handleLogOut.bind(this)}>Log Out</button><br/>
         This is *your* profile.
         { this.props.children }
       </div>
