dd0f81654b7286270f4f6a76ccfa09075921d0f5
joesass <dibson2013@gmail.com>
2017-02-21 15:57:55 -0500

    Add navbar and move account controls into it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router'
+import { Link, browserHistory } from 'react-router'
 import './App.css';
+import { Navbar, NavItem } from 'react-materialize';
+
 
 
 class App extends Component {
+  loggedIn(){
+    if (sessionStorage.jwt) {
+      return true
+    }
+    return false
+  }
+  handleLogOut(){
+    sessionStorage.clear()
+    browserHistory.push('/')
+  }
+
   render() {
+    let buttons = ''
+    if (this.loggedIn()){
+      buttons = <div><NavItem ><Link onClick={this.handleLogOut.bind(this)}>Log Out</Link></NavItem></div>
+    } else {
+      buttons = (<div><NavItem ><Link to={`/login`}>Log In</Link></NavItem>
+      <NavItem ><Link to={'/signup'}>Sign Up</Link></NavItem></div>)
+    }
     return (
       <div className="App">
-        <div className="App-header">
-          <nav>
-            <Link to={'/'} >MetroMeet Logo Here</Link>
-          </nav>
-        </div>
-        { this.props.children }
+          <Navbar brand='MetroMeet' right>
+            {buttons}
+          </Navbar>
+          <div className="container">
+            { this.props.children }
+          </div>
       </div>
     );
   }
--- src/components/Profile/ProfileApp.js
+++ src/components/Profile/ProfileApp.js
@@ -3,15 +3,9 @@ import { browserHistory } from 'react-router'
 
 class ProfileApp extends Component {
 
-  handleLogOut(){
-    sessionStorage.clear()
-    browserHistory.push('/')
-  }
-
   render(){
     return(
       <div className="Profile">
-        <button onClick={this.handleLogOut.bind(this)}>Log Out</button><br/>
         { this.props.children }
       </div>
     )
